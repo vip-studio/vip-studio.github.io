95c289d530c8cb3ede94127b67f044033ae3d735
Laurence Cliffe <loz@lozcliffe.com>
2023-03-14 09:51:47 +0000

    Creative studio project uploaded

---
+++ creative-studios/login-page.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+      window.open("profile.html", "_self");
+    } else {
+        loginErrorMsg.innerHTML = "Invalid username or password - please check your login details.";
+    }
+})
